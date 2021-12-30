0ebc7237cb5e7538e94dc79541ded0fe91d8d0b2
Muaaz <mgawan@lbl.gov>
2021-12-30 11:43:57 -0800

    added reservation name

--- CUDA/Ex-2/batch.sh
+++ CUDA/Ex-2/batch.sh
@@ -3,6 +3,7 @@
 #SBATCH -C gpu
 #SBATCH -G 4
 #SBATCH -t 5
-#SBATCH -A m3902_g
+#SBATCH -A ntrain3_g
+#SBATCH --reservation=perlmutter_day1
 
 ./vec_add
--- CUDA/Ex-5/script_close.sh
+++ CUDA/Ex-5/script_close.sh
@@ -2,11 +2,11 @@
 #SBATCH -N 1
 #SBATCH -C gpu
 #SBATCH -G 4
-#SBATCH -t 1
+#SBATCH -t 5
 #SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=2
-#SBATCH -A m3902_g
-#SBATCH --exclusive
+#SBATCH -A ntrain3_g
+#SBATCH --reservation=perlmutter_day1
 
 lscpu | grep NUMA
 echo -e "\n"
