e1d742b1dc073f38775c18f8c5212a98e29e2e2a
Steve Leak <sleak@lbl.gov>
2022-01-05 08:50:10 -0800

    add info options to openmp/openacc examples

--- OpenMP-OpenACC/Ex-1/batch.sh
+++ OpenMP-OpenACC/Ex-1/batch.sh
@@ -6,6 +6,11 @@
 #SBATCH -A ntrain3_g
 #SBATCH --reservation=perlmutter_day1
 
+# setting NVCOMPILER_ACC_NOTIFY at runtime will print extra info about
+# kernel launches and data transfers
+# see https://docs.nersc.gov/performance/readiness/#runtime-environment-variables
+export NVCOMPILER_ACC_NOTIFY=3
+
 ./vec_add.seq
 
 ./vec_add.openmp
